947a8aff647f1146c945ebdce6bd5aacf740737c
Shedriq <jerrydwain4@gmail.com>
2023-05-10 08:52:57 +0000

    task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Write a script that reads stdin line by line and computes metrics"""
+'''A script for parsing HTTP request logs.
+'''
 import re
 
 
 def extract_input(input_line):
-    """Extracts sections of a line of an HTTP request log.
-    """
+    '''Extracts sections of a line of an HTTP request log.
+    '''
     fp = (
         r'\s*(?P<ip>\S+)\s*',
         r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
@@ -28,21 +29,22 @@ def extract_input(input_line):
 
 
 def print_statistics(total_file_size, status_codes_stats):
-    """Prints the accumulated statistics of the HTTP request log."""
+    '''Prints the accumulated statistics of the HTTP request log.
+    '''
     print('File size: {:d}'.format(total_file_size), flush=True)
     for status_code in sorted(status_codes_stats.keys()):
         num = status_codes_stats.get(status_code, 0)
         if num > 0:
-            print('{:s}: {d}'.format(status_code, num), flush=True)
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
 
 
 def update_metrics(line, total_file_size, status_codes_stats):
-    """Updates the metrics from a given HTTP request log.
+    '''Updates the metrics from a given HTTP request log.
     Args:
         line (str): The line of input from which to retrieve the metrics.
     Returns:
         int: The new total file size.
-    """
+    '''
     line_info = extract_input(line)
     status_code = line_info.get('status_code', '0')
     if status_code in status_codes_stats.keys():
@@ -51,7 +53,8 @@ def update_metrics(line, total_file_size, status_codes_stats):
 
 
 def run():
-    """Starts the log parser"""
+    '''Starts the log parser.
+    '''
     line_num = 0
     total_file_size = 0
     status_codes_stats = {
@@ -79,5 +82,5 @@ def run():
         print_statistics(total_file_size, status_codes_stats)
 
 
-If __name__ == '__main__':
+if __name__ == '__main__':
     run()
